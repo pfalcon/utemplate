8c80eb97a8618e2eb62b9c3d3384ff5f7d94e957
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-01 19:21:35 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='utemplate',
+      version='0.2.1',
+      description="""Very lightweight, memory-efficient (uses generator
+protocol), dependency-free template engine (compiles to Python source).
+Particularly well suited for usage with MicroPython.org""",
+      url='https://github.com/pfalcon/utemplate',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      packages=['utemplate'])
