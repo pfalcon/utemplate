4a3c7adc605f4f643f8af88688b95fd65c39f539
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-18 01:13:53 +0300

    Release 0.2.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='utemplate',
-      version='0.2.5',
+      version='0.2.6',
       description="""Very lightweight, memory-efficient (uses generator
 protocol), dependency-free template engine (compiles to Python source).
 Particularly well suited for usage with MicroPython.org""",
