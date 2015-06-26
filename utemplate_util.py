23408f66aa1a410e8237dadb48130a9f7c42da40
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-06-27 01:30:45 +0300

    utemplate_util.py: Update for Loader's taking package argument.
    Also, make "compile" action store compiled patterns in "compiled" subdir,
    where compiled.Loader will look for it. Previous compile action which put
    put files in the current dir renamed to "rawcompile", it's useful for
    debugging.

--- utemplate_util.py
+++ utemplate_util.py
@@ -1,19 +1,31 @@
 import sys
+import os
 import _io as io
 import utemplate.source
 import utemplate.compiled
 
 
 cmd = sys.argv[1]
-if cmd == "compile":
+package = None
+
+if cmd == "rawcompile":
     out_name = sys.argv[2].replace(".", "_") + ".py"
     with open(sys.argv[2]) as f_in, open(out_name, "w") as f_out:
         c = utemplate.source.Compiler(f_in, f_out)
         c.compile()
 
+if cmd == "compile":
+    loader = utemplate.source.Loader(package, ".")
+    try:
+        os.unlink(loader.compiled_path(sys.argv[2]))
+    except:
+        pass
+    render = loader.load(sys.argv[2])
+
 elif cmd == "render":
-    render = utemplate.compiled.load(sys.argv[2])
-    for x in render():
+    loader = utemplate.compiled.Loader(package, ".")
+    render = loader.load(sys.argv[2])
+    for x in render(*sys.argv[3:]):
         sys.stdout.write(x)
 
 elif cmd == "run":
@@ -23,7 +35,7 @@ elif cmd == "run":
         c.compile()
     ns = {}
     exec(f_out.getvalue(), ns)
-    for x in ns["render"]():
+    for x in ns["render"](*sys.argv[3:]):
         sys.stdout.write(x)
 
 else:
