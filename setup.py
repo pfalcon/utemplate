2f5958d23b49db8e5716a79c1ac2b5d308d01cfc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-02-10 15:39:34 +0200

    setup: Release 1.2.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import optimize_upip
 
 
 setup(name='utemplate',
-      version='1.1',
-      description="""Very lightweight, memory-efficient (uses generator
-protocol), dependency-free template engine (compiles to Python source).
+      version='1.2',
+      description="""Very lightweight, memory-efficient (uses generator \
+protocol), dependency-free template engine (compiles to Python source). \
 Particularly well suited for usage with MicroPython.org""",
       url='https://github.com/pfalcon/utemplate',
       author='Paul Sokolovsky',
