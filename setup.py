f1b532c878cce63bd0cd6aa212b9d4ca3400d18c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-06-12 09:51:43 +0300

    setup.py: Release 1.3, use README.md as long_description.
    Also, simplify short description.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import sdist_upip
 
 
 setup(name='utemplate',
-      version='1.2',
-      description="""Very lightweight, memory-efficient (uses generator \
-protocol), dependency-free template engine (compiles to Python source). \
-Particularly well suited for usage with MicroPython.org""",
+      version='1.3',
+      description="""Very lightweight, memory-efficient, dependency-free template engine (compiles to Python source).""",
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       url='https://github.com/pfalcon/utemplate',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
